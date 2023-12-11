bd33d291c9a29ad9845186a2baea9347a7c80707
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2023-12-11 17:13:30 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const questions = [];
+for (let i = 1; i <= 5; i++) {
+    questions[i - 1] = document.querySelector(`label.label${i}`);
+}
+
+const answers = [];
+for (let i = 1; i <= 5; i++) {
+    answers[i - 1] = document.getElementById(`answer${i}`);
+}
+
+const imagens = [];
+for (let i = 1; i < 5; i++) {
+    imagens[i - 1] = document.getElementById(`img-plus${i}`)    
+}
+
+const checkbox = [];
+for (let i = 1; i < 5; i++) {
+    checkbox[i - 1] = document.getElementById(`plus${i}`)    
+}
+
+function mostrar(pergunta, check, source, height) {
+    if (checkbox[check].checked) {
+        imagens[pergunta].src = source;
+        answers[pergunta].style.height = height;
+    } else {
+        imagens[pergunta].src = 'assets/images/icon-plus.svg';
+        answers[pergunta].style.height = '0';  
+    }
+}
